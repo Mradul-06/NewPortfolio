46b55bc14ca0db25a9cccbd8555a87b7fe6d8550
Mradul Agrawal <mradulagrawal126@gmail.com>
2022-09-11 12:43:50 +0530

    mark15 arranged

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,6 +9,67 @@ const Projects = () => {
         <h2> NeoG - Projects</h2>
       </div>
     <div className="card-holder">
+-----------------------------------
+    <div className="card">
+        <div className="card-heading">
+          <h4>Do You Know Me Quiz</h4>
+        </div>
+        <hr></hr>
+        <div className="card-content">
+        <p> This is a CLI quiz</p>
+        </div> 
+        <hr></hr>
+        <div className="card-links">
+            <a className="project-link" href="https://replit.com/@MradulAgrawal/Do-You-Know-Me-quiz?v=1">Visit</a><br></br>
+            <a className="source-link" href="https://github.com/Mradul-06/Do-You-Know-Me-Quiz">Github</a>
+        </div>     
+      </div>
+--------------------------------------
+      <div className="card">
+        <div className="card-heading">
+          <h4>Fruits Quiz</h4>
+        </div>
+        <hr></hr>
+        <div className="card-content">
+        <p> This is a CLI quiz to check your Knowledge about fruits</p>
+        </div> 
+        <hr></hr>
+        <div className="card-links">
+            <a className="project-link" href="https://replit.com/@MradulAgrawal/Fruit-Quiz?v=1">Visit</a><br></br>
+            <a className="source-link" href="https://github.com/Mradul-06/Fruit-Quiz-CLI">Github</a>
+        </div>     
+      </div>
+---------------------------------------
+      <div className="card">
+        <div className="card-heading">
+          <h4>Minion Language Translator</h4>
+        </div>
+        <hr></hr>
+        <div className="card-content">
+        <p> This WebApp Converts English To Minions Language</p>
+        </div> 
+        <hr></hr>
+        <div className="card-links">
+            <a className="project-link" href="https://banana-speak-mradul.netlify.app">Visit</a><br></br>
+            <a className="source-link" href="https://github.com/Mradul-06/Banana-Speak">Github</a>
+        </div>     
+      </div>
+---------------------------------------
+<div className="card">
+        <div className="card-heading">
+          <h4>Yoda Translate</h4>
+        </div>
+        <hr></hr>
+        <div className="card-content">
+        <p> Use this tool to translate english in yoda language</p>
+        </div> 
+        <hr></hr>
+        <div className="card-links">
+            <a className="project-link" href="https://thirsty-perlman-0cc4b1.netlify.app">Visit</a><br></br>
+            <a className="source-link" href="https://github.com/Mradul-06/translation_app">Github</a>
+        </div>     
+      </div>
+-------------------------------------------------
       <div className="card">
         <div className="card-heading">
           <h4>Emoji Interpretor</h4>
@@ -24,90 +85,7 @@ const Projects = () => {
             <a className="source-link" href="https://github.com/Mradul-06/emojiInterpretor">Github</a>
         </div>     
       </div>
-      <div className="card">
-        <div className="card-heading">
-          <h4>Fun-With-Triangles</h4>
-        </div>
-        <hr></hr>
-        <div className="card-content">
-        <p> This WebApp is a Quiz to check your Knowledge on Triangles.</p>
-        </div> 
-        <hr></hr>
-        <div className="card-links">
-            <a className="project-link" href="https://hardcore-bell-cc882c.netlify.app/">Visit</a><br></br>
-            <a className="source-link" href="https://github.com/Mradul-06/Fun-With-triangles">Github</a>
-        </div>     
-      </div>
-      <div className="card">
-        <div className="card-heading">
-          <h4>IS Your Birthday Lucky</h4>
-        </div>
-        <hr></hr>
-        <div className="card-content">
-        <p> This WebApp check Your Birthday date With Your Lucky Number</p>
-        </div> 
-        <hr></hr>
-        <div className="card-links">
-            <a className="project-link" href="https://brave-shannon-33a476.netlify.app/">Visit</a><br></br>
-            <a className="source-link" href="https://github.com/Mradul-06/Is-Your-Birtday-Lucky">Github</a>
-        </div>     
-      </div>
-      <div className="card">
-        <div className="card-heading">
-          <h4>Minion Language Translator</h4>
-        </div>
-        <hr></hr>
-        <div className="card-content">
-        <p> This WebApp Converts English To Minions Language</p>
-        </div> 
-        <hr></hr>
-        <div className="card-links">
-            <a className="project-link" href="https://banana-speak-mradul.netlify.app">Visit</a><br></br>
-            <a className="source-link" href="https://github.com/Mradul-06/Banana-Speak">Github</a>
-        </div>     
-      </div>
-      <div className="card">
-        <div className="card-heading">
-          <h4>Profit Loss Calculator</h4>
-        </div>
-        <hr></hr>
-        <div className="card-content">
-        <p> This WebApp allows you to calculate Profilt and Loss based on Buy and Sell Price</p>
-        </div> 
-        <hr></hr>
-        <div className="card-links">
-            <a className="project-link" href="https://musing-shockley-3a970c.netlify.app/">Visit</a><br></br>
-            <a className="source-link" href="https://github.com/Mradul-06/ProfitAndLossCalculator">Github</a>
-        </div>     
-      </div>
-      <div className="card">
-        <div className="card-heading">
-          <h4>YT Video Player</h4>
-        </div>
-        <hr></hr>
-        <div className="card-content">
-        <p> Browse Youtube Videos</p>
-        </div> 
-        <hr></hr>
-        <div className="card-links">
-            <a className="project-link" href="https://glowing-starlight-6ec35c.netlify.app/">Visit</a><br></br>
-            <a className="source-link" href="https://github.com/Mradul-06/YT-Video-Player">Github</a>
-        </div>     
-      </div>
-      <div className="card">
-        <div className="card-heading">
-          <h4>Yoda Translate</h4>
-        </div>
-        <hr></hr>
-        <div className="card-content">
-        <p> Use this tool to translate english in yoda language</p>
-        </div> 
-        <hr></hr>
-        <div className="card-links">
-            <a className="project-link" href="https://thirsty-perlman-0cc4b1.netlify.app">Visit</a><br></br>
-            <a className="source-link" href="https://github.com/Mradul-06/translation_app">Github</a>
-        </div>     
-      </div>
+-----------------------------------------
       <div className="card">
         <div className="card-heading">
           <h4>Food Recommendation</h4>
@@ -122,35 +100,10 @@ const Projects = () => {
             <a className="source-link" href="https://github.com/Mradul-06/food-app">Github</a>
         </div>     
       </div>
-      <div className="card">
-        <div className="card-heading">
-          <h4>Fruits Quiz</h4>
-        </div>
-        <hr></hr>
-        <div className="card-content">
-        <p> This is a CLI quiz to check your Knowledge about fruits</p>
-        </div> 
-        <hr></hr>
-        <div className="card-links">
-            <a className="project-link" href="https://replit.com/@MradulAgrawal/Fruit-Quiz?v=1">Visit</a><br></br>
-            <a className="source-link" href="https://github.com/Mradul-06/Fruit-Quiz-CLI">Github</a>
-        </div>     
-      </div>
-      <div className="card">
-        <div className="card-heading">
-          <h4>Do You Know Me Quiz</h4>
-        </div>
-        <hr></hr>
-        <div className="card-content">
-        <p> This is a CLI quiz</p>
-        </div> 
-        <hr></hr>
-        <div className="card-links">
-            <a className="project-link" href="https://replit.com/@MradulAgrawal/Do-You-Know-Me-quiz?v=1">Visit</a><br></br>
-            <a className="source-link" href="https://github.com/Mradul-06/Do-You-Know-Me-Quiz">Github</a>
-        </div>     
-      </div>
-      <div className="card">
+---------------------------------------------
+
+
+<div className="card">
         <div className="card-heading">
           <h4>Cash Register Manager</h4>
         </div>
@@ -164,7 +117,38 @@ const Projects = () => {
             <a className="source-link" href="https://github.com/Mradul-06/cash-register-manager">Github</a>
         </div>     
       </div>
+--------------------------------------------
+<div className="card">
+        <div className="card-heading">
+          <h4>IS Your Birthday Lucky</h4>
+        </div>
+        <hr></hr>
+        <div className="card-content">
+        <p> This WebApp check Your Birthday date With Your Lucky Number</p>
+        </div> 
+        <hr></hr>
+        <div className="card-links">
+            <a className="project-link" href="https://brave-shannon-33a476.netlify.app/">Visit</a><br></br>
+            <a className="source-link" href="https://github.com/Mradul-06/Is-Your-Birtday-Lucky">Github</a>
+        </div>     
+      </div>
+-----------------------------------------------
       <div className="card">
+        <div className="card-heading">
+          <h4>Fun-With-Triangles</h4>
+        </div>
+        <hr></hr>
+        <div className="card-content">
+        <p> This WebApp is a Quiz to check your Knowledge on Triangles.</p>
+        </div> 
+        <hr></hr>
+        <div className="card-links">
+            <a className="project-link" href="https://hardcore-bell-cc882c.netlify.app/">Visit</a><br></br>
+            <a className="source-link" href="https://github.com/Mradul-06/Fun-With-triangles">Github</a>
+        </div>     
+      </div>
+-------------------------------------------
+<div className="card">
         <div className="card-heading">
           <h4>Pallindrome Birthday</h4>
         </div>
@@ -178,6 +162,40 @@ const Projects = () => {
             <a className="source-link" href="https://github.com/Mradul-06/pallindrome-birthday">Github</a>
         </div>     
       </div>
+------------------------------------------------
+      <div className="card">
+        <div className="card-heading">
+          <h4>Profit Loss Calculator</h4>
+        </div>
+        <hr></hr>
+        <div className="card-content">
+        <p> This WebApp allows you to calculate Profilt and Loss based on Buy and Sell Price</p>
+        </div> 
+        <hr></hr>
+        <div className="card-links">
+            <a className="project-link" href="https://musing-shockley-3a970c.netlify.app/">Visit</a><br></br>
+            <a className="source-link" href="https://github.com/Mradul-06/ProfitAndLossCalculator">Github</a>
+        </div>     
+      </div>
+--------------------------------------------------
+      <div className="card">
+        <div className="card-heading">
+          <h4>YT Video Player</h4>
+        </div>
+        <hr></hr>
+        <div className="card-content">
+        <p> Browse Youtube Videos</p>
+        </div> 
+        <hr></hr>
+        <div className="card-links">
+            <a className="project-link" href="https://glowing-starlight-6ec35c.netlify.app/">Visit</a><br></br>
+            <a className="source-link" href="https://github.com/Mradul-06/YT-Video-Player">Github</a>
+        </div>     
+      </div>
+----------------------------------------------------
+
+
+
     </div>
     </section>
   )
